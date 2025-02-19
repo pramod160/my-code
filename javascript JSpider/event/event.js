eb94fe4cc01aab7ff356219f5540dbb27ff5647d
pramod160 <prajwalalaladinni8@gmail.com>
2025-02-19 22:06:35 +0530

    Add files via upload

---
+++ javascript JSpider/event/event.js
@@ -0,0 +1,100 @@
+// //! onlcick event
+// function red(){
+//     console.log("hi i ak red");
+//     document.body.style.background="red";
+// }
+
+// function yellow(){
+//     console.log("hi i ak yellow");
+//     document.body.style.background="yellow";
+// }
+
+// //! onDoubleClick()
+
+// function onDbClick(){
+//     console.log("I ama double click event");
+//     let OTP = Math.floor(Math.random()*(999-100)+100);
+//     alert(`OTP is created successfully :${OTP}`);
+// }
+
+
+// let inputKey = document.getElementById("inputKey");
+// console.log(inputKey);
+
+// function keyBoardfunc(){
+//     inputKey.style.background="yellow";
+//     document.body.style.backgroundColor =`#${Math.floor(Math.random()*9999)}`;
+// }
+
+// function keyBoardfunc1(){
+//     inputKey.style.background="red";
+//     document.body.style.backgroundColor =`#${Math.floor(Math.random()*9999)}`;
+// }
+
+
+// let section = document.getElementsByClassName("sectionTag");
+// console.log(section) ;
+
+// let array = Array.from(section);
+// array.map((m)=>{
+//     console.log(m);
+//     m.addEventListener("mouseover", ()=>{
+//         m.style.background = m.innerHTML;
+//     })
+
+//     m.addEventListener("mouseout", ()=>{
+//         m.style.background = "initial";
+//     })
+// })
+
+// ____________________________________________
+
+// !self practice 
+// onClick
+// function red (){
+//     document.body.style.background = "red";
+// }
+
+// function yellow(){
+//     document.body.style.background ="yellow";
+// }
+
+// onDoubleClick
+
+// function onDbClick(){
+//     document.body.style.background = "blue";
+//     let OTP = Math.floor(Math.random()*(999-100)+100);
+//     console.log(OTP);
+// }
+
+// let prajwal = document.getElementById("inputKey");
+// console.log(prajwal);
+
+// function keyBoardfunc(){
+//     prajwal.style.background = "lightGreen";
+//     document.body.style.background = `#${Math.floor(Math.random()*9999)}`;
+// }
+// function keyBoardfunc1(){
+//     prajwal.style.background = "red";
+//     document.body.style.background = `#${Math.floor(Math.random()*9999)}`;
+// }
+
+// mouseover and mouseout
+
+// let hlo = document.getElementsByClassName("sectionTag");
+// console.log(hlo);
+
+// let hello = Array.from(hlo);
+// console.log(hello)
+
+// hello.map((m)=>{
+// // console.log(m);
+// m.addEventListener("mouseover", ()=>{
+//     m.style.background = m.innerHTML;
+// })
+
+// m.addEventListener("mouseout", ()=>{
+//     m.style.background = "initial";
+// })
+// })
+
